36618d4e269f791a9ba8e74cba73883fa1d93000
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-03-15 17:42:23 +0100

    Small refactor of SourceCode component

--- components/SourceCode/SourceCode.js
+++ components/SourceCode/SourceCode.js
@@ -10,14 +10,28 @@ const printConst = (type, constName) => {
   return `const ${constName} = ${value};`
 }
 
+const getBuildParamsExplanation = (type, buildParams) => {
+  return buildParams.map(constName => printConst(type, constName)).join('\n');
+}
+
 const getSourceCodeExampleForType = (type) => {
   const { buildParams, typeName } = type;
   const fnName = typeName.substr(0, 1).toLowerCase() + typeName.substr(1);
-  if (buildParams) {
-    const argsDefinitions = buildParams.map(constName => printConst(type, constName)).join('\n');
-    return `${argsDefinitions}\n\nconst node = jscodeshift.${fnName}(${buildParams.join(', ')});`;
+  const sourceCode = [];
+  let params = '';
+
+  sourceCode.push('import astTypes from \'ast-types\';');
+  sourceCode.push('\n');
+
+  if (buildParams.length) {
+    params = buildParams.join(', ');
+    sourceCode.push(getBuildParamsExplanation(type, buildParams));
+    sourceCode.push('\n');
   }
-  return `const node = jscodeshift.${fnName}();`;
+
+  sourceCode.push(`const node = astTypes.builders.${fnName}(${params});`);
+
+  return sourceCode.join('\n');
 }
 
 export default ({ type }) => (
