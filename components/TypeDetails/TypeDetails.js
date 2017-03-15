f19a4c3c09eb304881d6f86c9d3e83c20373e480
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-03-15 18:06:42 +0100

    Removed fields from TypeDetails

--- components/TypeDetails/TypeDetails.js
+++ components/TypeDetails/TypeDetails.js
@@ -10,32 +10,14 @@ const renderFieldValue = (value) => {
   return JSON.stringify(value);
 }
 
-const renderField = ({ name, value, hidden }) => {
-  const hiddenEmblem = <span className="field-hidden">(hidden)</span>;
-  return <li key={name} className="fieldsList-item"><label className="field-name">{name}</label> <span className="field-value">{renderFieldValue(value)} {hidden ? hiddenEmblem : ''}</span></li>;
-}
-
-const renderFields = (type) => {
-  const { allFields } = type;
-  if (allFields.length === 0) {
-    return <em>None</em>;
-  }
-  return Object.keys(allFields).map(fieldName => renderField(allFields[fieldName]));
-}
-
 const isBuildable = (type) => type.buildable;
 
 export default ({ type, astTypes }) => (
   <div className="typeDetails">
     <h3 className="typeDetails-heading">{type.typeName}</h3>
-
-    <h4 className="typeDetails-subHeading">Base</h4>
-    <SuperTypes type={type} astTypes={astTypes} />
-
-    <h4 className="typeDetails-subHeading">Fields</h4>
-    <ul className="fieldsList">
-      { renderFields(type) }
-    </ul>
+    <nav className="typeDetails-breadcrumb">
+      <SuperTypes type={type} astTypes={astTypes} />
+    </nav>
 
     <h4 className="typeDetails-subHeading">Building</h4>
     {
