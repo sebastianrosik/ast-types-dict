7fdcf544aae1da8a1c3a5249edc9c4d20857edc3
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-03-16 21:38:16 +0100

    Better look for Details pane

--- components/TypeDetails/TypeDetails.js
+++ components/TypeDetails/TypeDetails.js
@@ -14,12 +14,12 @@ const isBuildable = (type) => type.buildable;
 
 export default ({ type, astTypes }) => (
   <div className="typeDetails">
-    <h3 className="typeDetails-heading">{type.typeName}</h3>
-    <nav className="typeDetails-breadcrumb">
-      <SuperTypes type={type} astTypes={astTypes} />
-    </nav>
-
-    <h4 className="typeDetails-subHeading">Building</h4>
+    <header className="typeDetails-header">
+        <h3 className="typeDetails-heading">{type.typeName}</h3>
+        <nav className="typeDetails-breadcrumb">
+          <SuperTypes type={type} astTypes={astTypes} />
+        </nav>
+    </header>
     {
       isBuildable(type)
       ?
